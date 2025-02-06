41d27d61b849d896b7585c7b42541ef54f547dab
superp00t <superp00t@tutanota.com>
2025-02-06 14:11:46 -0500

    fix(build): remove gopsutil

--- faws/multipart/buffer_size.go
+++ faws/multipart/buffer_size.go
@@ -1,22 +1,9 @@
 package multipart
 
-import (
-	"fmt"
-
-	"github.com/shirou/gopsutil/v4/mem"
-)
-
 const default_bs = 16384
 
 // returns a good buffering size for the current machine
 func good_buffer_size() (bs int) {
-	bs = default_bs
-	v, err := mem.VirtualMemory()
-	if err != nil {
-		return
-	}
-	bs = min(int(v.Free/32), int(v.Total/64))
-
-	fmt.Println("buffer size", bs)
+	bs = default_bs * 1000
 	return
 }
